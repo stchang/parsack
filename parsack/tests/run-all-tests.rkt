3265cbe3082822398ab3670b85407805efcfcd28
Stephen Chang <stchang@ccs.neu.edu>
2013-11-07 15:37:51 -0500

    add run-all-tests.rkt

---
+++ parsack/tests/run-all-tests.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(require "parsack-tests.rkt")
+(require "../examples/tests/csv-parser-basic-tests.rkt")
+(require "../examples/tests/csv-parser-quoted-tests.rkt")
+(require "../examples/tests/csv-parser-sepBy-tests.rkt")
+(require "../examples/tests/http-request-parser-tests.rkt")
+(require "../examples/tests/json-parser-tests.rkt")
+(require "../examples/tests/url-query-parser-tests.rkt")
