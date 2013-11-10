10f587458215e72858ab9b209cbba81b48091bae
Stephen Chang <stchang216@gmail.com>
2013-11-10 18:38:15 -0500

    update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,2 +1,4 @@
-#lang setup/infotab
+#lang info
 (define collection 'multi)
+(define deps '("base"))
+(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib"))
---
+++ parsack/info.rkt
@@ -0,0 +1,2 @@
+#lang info
+(define scribblings '(("parsack.scrbl" ())))
