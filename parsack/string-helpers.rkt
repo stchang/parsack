3a02d3788b7bb5d6b4a05b3b2651d9309005c0fd
Stephen Chang <stchang@ccs.neu.edu>
2015-11-30 17:47:07 -0500

    delete string-helpers file

--- parsack/string-helpers.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket/base
-(provide (all-defined-out))
-(define (str-empty? str) (string=? str ""))
-(define (str-fst str) (string-ref str 0))
-(define (str-rst str) (substring str 1))
-(define mk-string string)
